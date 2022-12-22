60135274b08de6f61d669e6e35b8a124e7783445
maayan <maayanramot1@gmail.com>
2022-12-22 10:30:32 +0200

    dark mode commit

--- main.js
+++ main.js
@@ -13,6 +13,20 @@ function delayHandleClick(handleClick) {
     setTimeout(handleClick2(),3000);
 }
 
+function indicateDarkMode() {
+    let backgroundColor = window.getComputedStyle(document.body ,null).getPropertyValue('background-color');
+    let textColor = window.getComputedStyle(document.body ,null).getPropertyValue('color');
+    if (backgroundColor == 'rgb(255, 255, 255)' && textColor == 'rgb(0, 0, 0)') {
+        console.log('Dark mode is enable');
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+    } else {
+        console.log('Dark mode is disabled');
+        document.body.style.backgroundColor = "rgb(255, 255, 255)";
+        document.body.style.color = "black";
+    }
+}
+
 
 
 
