2d4222fb6a40981dc0863634eafc302ad46a7bfa
maayan <maayanramot1@gmail.com>
2023-01-02 12:11:34 +0200

    change img commit

--- main.js
+++ main.js
@@ -27,6 +27,51 @@ function indicateDarkMode() {
     }
 }
 
+function changeList() {
+    const favoriteFood = ["Raviolli", "Sushi", "Chocolate", "strawberry"];
+    let makeList = '<ul>';
+    for (let index = 0; index < favoriteFood.length; index++) {
+        const element = favoriteFood[index];
+        makeList += `<li> ${element} </li>`;
+    }
+    makeList += '</ul>';
+    console.log(makeList);
+    document.getElementById("favoriteFood").outerHTML = makeList;
+}
+// changeList();
+
+let imagesDB = [
+    { description : "Goni" , imgURL : "/AboutMe/assets/goni.jpg", alt : "img1" },
+    { description : "Diving" , imgURL : "/AboutMe/assets/diving.jpg", alt : "img2" },
+    { description : "Dog" , imgURL : "/AboutMe/assets/dog.jpg", alt : "img3" }
+];
+
+function returnHTML(array) {
+    let gallery = document.createElement("div");
+    gallery.classList.add("gallery");
+
+    for ( let i = 0; i < imagesDB.length; i++ ) {
+        let img = document.createElement("img");
+        img.classList.add("images");
+        img.src = imagesDB[i].src;
+        img.alt = imagesDB[i].alt;
+
+        let desc = document.createElement("p");
+        desc.textContent = imagesDB[i].description;
+        desc.classList.add("descImg");
+
+        gallery.appendChild(img);
+        gallery.appendChild(desc);
+
+    }
+    document.body.appendChild(gallery);
+    return gallery;
+}
+
+
+
+
+
 
 
 
